b038db337755fcb8532d011ebc97256aab782c09
Rsanjana37 <sanjr.krishnan@gmail.com>
2025-01-26 22:26:14 +0530

    modified screens

--- lib/screens/itinerary_screen.dart
+++ lib/screens/itinerary_screen.dart
@@ -1,42 +1,123 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import '../models/trip.dart';
 import 'day_planner_screen.dart';
 
 class ItineraryScreen extends StatelessWidget {
   final Trip trip;
+  final List<String> imageUrls = [
+    'https://t4.ftcdn.net/jpg/01/16/61/93/360_F_116619399_YA611bKNOW35ffK0OiyuaOcjAgXgKBui.jpg',
+    'https://t3.ftcdn.net/jpg/01/76/33/14/360_F_176331484_nLHY9EoW0ETwPZaS9OBXPGbCJhT70GZe.jpg',
+    'https://lajolla.com/nitropack_static/qjIMPjNnWKMPNOkdSPGMqXckjXGVLtvg/assets/images/optimized/rev-a739b3e/lajolla.com/wp-content/uploads/2021/06/Best-San-Diego-Hotels-With-Pools-Your-Ultimate-Guide-1024x576.jpg',
+    'https://thumbs.dreamstime.com/b/breathtaking-tropical-beach-scene-swaying-palm-trees-mesmerizing-sunset-330195710.jpg',
+    'https://images.unsplash.com/photo-1724398932228-7acec186bdc2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3',
+    'https://img-cdn.inc.com/image/upload/f_webp,q_auto,c_fit/images/panoramic/getty_522735456_249841.jpg',
+  ];
 
   ItineraryScreen({required this.trip});
 
+  String getRandomImage() {
+    final random = Random();
+    return imageUrls[random.nextInt(imageUrls.length)];
+  }
+
+  String _getActivityPreview(List<Activity> activities) {
+    if (activities.isEmpty) {
+      return 'Tap to add activities';
+    } else if (activities.length == 1) {
+      return '1 activity planned';
+    } else {
+      return '${activities.length} activities planned';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final randomImage = getRandomImage();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Itinerary: ${trip.name}'),
-      ),
-      body: ListView.builder(
-        itemCount: trip.endDate.difference(trip.startDate).inDays + 1,
-        itemBuilder: (context, index) {
-          final currentDate = trip.startDate.add(Duration(days: index));
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
               title: Text(
-                  'Day ${index + 1}: ${currentDate.toString().substring(0, 10)}'),
-              subtitle: Text('Tap to plan activities'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DayPlannerScreen(
-                      trip: trip,
-                      day: currentDate,
+                'Itinerary',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 3.0,
+                      color: Color.fromARGB(255, 0, 0, 0),
                     ),
+                  ],
+                ),
+              ),
+              background: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(randomImage),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          Colors.black.withOpacity(0.7),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final currentDate = trip.startDate.add(Duration(days: index));
+                final activities = trip.getActivitiesForDay(currentDate);
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: ListTile(
+                    title: Text(
+                      'Day ${index + 1}: ${currentDate.toString().substring(0, 10)}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(_getActivityPreview(activities)),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DayPlannerScreen(
+                            trip: trip,
+                            day: currentDate,
+                          ),
+                        ),
+                      ).then((_) {
+                        // Refresh the screen when returning from DayPlannerScreen
+                        (context as Element).markNeedsBuild();
+                      });
+                    },
+                    trailing: Icon(Icons.arrow_forward_ios),
                   ),
                 );
               },
+              childCount: trip.endDate.difference(trip.startDate).inDays + 1,
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
--- lib/screens/suggestions_screen.dart
+++ lib/screens/suggestions_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import '../models/trip.dart';
 
@@ -6,23 +7,122 @@ class SuggestionsScreen extends StatelessWidget {
 
   SuggestionsScreen({required this.trip});
 
+  // Static data for accommodations, restaurants, and attractions with ratings
+  final List<Map<String, dynamic>> accommodations = [
+    {
+      'imageUrl':
+          'https://www.theindia.co.in/blog/wp-content/uploads/2024/07/praveg-ghoghla-beach-resort-diu-1200x676.jpg',
+      'name': 'Amazing Resort',
+      'rating': 5,
+    },
+    {
+      'imageUrl':
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/173021092.jpg?k=cac3018d002a6850c92a989ee64709157635c82dea9d9ee97cda97c9d90defdf&o=&hp=1',
+      'name': 'Wonderful Guesthouse',
+      'rating': 4,
+    },
+    {
+      'imageUrl':
+          'https://cdn.pixabay.com/photo/2023/02/06/17/11/hotelroom-7772422_1280.jpg',
+      'name': 'Fantastic Hotel',
+      'rating': 3,
+    },
+    {
+      'imageUrl':
+          'https://media.architecturaldigest.com/photos/585ab4aa1f906f61574e6078/16:9/w_2580,c_limit/william-vale-penthouse-04.jpg',
+      'name': 'Delightful Hotel',
+      'rating': 5,
+    },
+  ];
+
+  final List<Map<String, dynamic>> restaurants = [
+    {
+      'imageUrl':
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/74/49/bb/bazaar.jpg',
+      'name': 'Fantastic Bistro',
+      'rating': 4,
+    },
+    {
+      'imageUrl':
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/74/49/38/jhind.jpg?w=600&h=-1&s=1',
+      'name': 'Wonderful Diner',
+      'rating': 3,
+    },
+    {
+      'imageUrl':
+          'https://i.natgeofe.com/n/04cf2a79-4a49-45eb-90f8-38356167690d/image00037.jpeg',
+      'name': 'Delightful Bistro',
+      'rating': 5,
+    },
+    {
+      'imageUrl':
+          'https://b.zmtcdn.com/data/collections/a7bf69f0d955766f9fe5ac026c2eaec0_1709815455.jpg',
+      'name': 'Tres Gatos',
+      'rating': 4,
+    },
+  ];
+
+  final List<Map<String, dynamic>> attractions = [
+    {
+      'imageUrl':
+          'https://www.columbiacommunities.in/wp-content/uploads/2023/12/shutterstock_1938868960-scaled.jpg',
+      'name': 'Lovely Beach',
+      'rating': 5,
+    },
+    {
+      'imageUrl':
+          'https://montgomeryparks.org/wp-content/uploads/2016/07/aberdeen-park-playground-e1702563603419.jpg',
+      'name': 'Amazing Park',
+      'rating': 3,
+    },
+    {
+      'imageUrl':
+          'https://www.nelincs.gov.uk/assets/uploads/2024/01/Weelsby-woods-area-page-1024x683.jpg',
+      'name': 'Wonderful Parklands',
+      'rating': 4,
+    },
+    {
+      'imageUrl':
+          'https://media.springernature.com/full/springer-static/image/art%3A10.1038%2Fs41477-019-0374-3/MediaObjects/41477_2019_374_Figa_HTML.jpg',
+      'name': 'Charming Forest',
+      'rating': 5,
+    },
+  ];
+
+  // Function to get random suggestions
+  List<Map<String, dynamic>> _getRandomSuggestions(
+      List<Map<String, dynamic>> data) {
+    final random = Random();
+    final randomIndexes = <int>{};
+    while (randomIndexes.length < 2) {
+      randomIndexes.add(random.nextInt(data.length));
+    }
+    return randomIndexes.map((index) => data[index]).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final randomAccommodations = _getRandomSuggestions(accommodations);
+    final randomRestaurants = _getRandomSuggestions(restaurants);
+    final randomAttractions = _getRandomSuggestions(attractions);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Suggestions: ${trip.name}'),
       ),
       body: ListView(
         children: [
-          _buildSuggestionCategory(context, 'Accommodations'),
-          _buildSuggestionCategory(context, 'Restaurants'),
-          _buildSuggestionCategory(context, 'Attractions'),
+          _buildSuggestionCategory(
+              context, 'Accommodations', randomAccommodations),
+          _buildSuggestionCategory(context, 'Restaurants', randomRestaurants),
+          _buildSuggestionCategory(context, 'Attractions', randomAttractions),
         ],
       ),
     );
   }
 
-  Widget _buildSuggestionCategory(BuildContext context, String category) {
+  Widget _buildSuggestionCategory(BuildContext context, String category,
+      List<Map<String, dynamic>> suggestions) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -36,9 +136,11 @@ class SuggestionsScreen extends StatelessWidget {
         ListView.builder(
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
-          itemCount: trip.suggestions[category]!.length,
+          itemCount: suggestions.length,
           itemBuilder: (context, index) {
-            final suggestion = trip.suggestions[category]![index];
+            final suggestion = suggestions[index];
+            final rating = suggestion['rating'];
+
             return Card(
               margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Column(
@@ -48,7 +150,7 @@ class SuggestionsScreen extends StatelessWidget {
                     borderRadius:
                         BorderRadius.vertical(top: Radius.circular(4)),
                     child: Image.network(
-                      suggestion.imageUrl,
+                      suggestion['imageUrl']!,
                       height: 200,
                       width: double.infinity,
                       fit: BoxFit.cover,
@@ -60,7 +162,7 @@ class SuggestionsScreen extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          suggestion.name,
+                          suggestion['name']!,
                           style: Theme.of(context).textTheme.titleMedium,
                         ),
                         SizedBox(height: 8),
@@ -68,21 +170,15 @@ class SuggestionsScreen extends StatelessWidget {
                           children: [
                             ...List.generate(5, (index) {
                               return Icon(
-                                index < suggestion.rating.floor()
-                                    ? Icons.star
-                                    : index < suggestion.rating
-                                        ? Icons.star_half
-                                        : Icons.star_border,
+                                index < rating ? Icons.star : Icons.star_border,
                                 color: Colors.amber,
                                 size: 18,
                               );
                             }),
                             SizedBox(width: 8),
-                            Text(suggestion.rating.toStringAsFixed(1)),
+                            Text(rating.toString()),
                           ],
                         ),
-                        SizedBox(height: 8),
-                        Text(suggestion.description),
                       ],
                     ),
                   ),
